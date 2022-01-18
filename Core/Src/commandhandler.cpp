891a17eaa4435f7e03e137bdc098485ce70fbb24
Freshdouble <peterkremsner@gmx.at>
2022-01-18 22:19:27 +0100

    Update description

--- Core/Src/commandhandler.cpp
+++ Core/Src/commandhandler.cpp
@@ -32,18 +32,19 @@ extern "C" {
 
 signed char SMPReceiveFunc(uint8_t *data, uint32_t length)
 {
-	auto buffer = smp.GetBuffer();
-	auto [command, offset, remainingbytes] = GetMatchedCommand(buffer, length);
+	auto buffer = smp.GetBuffer(); //Get the smp receive buffer. We can use this buffer directly because the smp receiver starts writing at the base address.
+	//TODO: Evaluate iterator based command matching to be independendt of the actual write position of libsmp
+	auto [command, offset, remainingbytes] = GetMatchedCommand(buffer, length); //Get the command that matches the data header
 
 	switch(command)
 	{
 	case Commands::SetLEDStatus:
 	{
 		SetLEDStatus ledstatus;
-		bool valid = std::get<1>(ledstatus.Unserialize<buffer.max_size()>(buffer.begin() + offset, buffer.end(), remainingbytes));
+		bool valid = std::get<1>(ledstatus.Unserialize<buffer.max_size()>(buffer.begin() + offset, buffer.end(), remainingbytes)); //Check if the data is valid. If this is false it is possible that the generated xcode file is older than the latest transmission.xml definition file
 		if(valid)
 		{
-			HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, ledstatus.argument.Read_ledstatus() ? GPIO_PIN_SET : GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, ledstatus.argument.Read_ledstatus() ? GPIO_PIN_SET : GPIO_PIN_RESET); //Set the output according to the command
 		}
 		break;
 	}
@@ -63,13 +64,13 @@ void loop()
 {
 	static uint32_t lastTimestamp = 0;
 	LEDStatus status;
-	if(HAL_GetTick() > (lastTimestamp + 1000))
+	if(HAL_GetTick() > (lastTimestamp + 1000)) //Check if 1s passed in a non blocking way so we don't stall the uart reception
 	{
 		bool led = HAL_GPIO_ReadPin(LD2_GPIO_Port, LD2_Pin) == GPIO_PIN_SET;
 		status.argument.Write_ledstatus(led);
 		std::array<uint8_t, LEDStatus::packetLength> data;
-		auto size = status.SerializeWithID(data);
-		smp.SendData<LEDStatus::packetLength, sendOverUART>(data, size);
+		auto size = status.SerializeWithID(data); //Serialize the data to the packet
+		smp.SendData<LEDStatus::packetLength, sendOverUART>(data, size); //Transmit the packet using the sendOverUART function.
 		lastTimestamp = HAL_GetTick();
 	}
 }
